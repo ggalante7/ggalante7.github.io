3da30e63d0853b7c7fe7822aaa481f50ea755268
Gabriel Greche Marim Galante <31481035+ggalante7@users.noreply.github.com>
2024-09-18 17:20:40 +0000

    feat: :tada:add gitignore and improve some stuffs

--- api.service.ts
+++ api.service.ts
@@ -1,9 +1,21 @@
-import { axios } from "axios";
+import { 
+    Axios
+} from "axios";
 
 
 export class ApiService {
     public baseUrl
+    public axios = Axios;
+    
     constructor(params) {
-        
+        this.baseUrl = params.baseUrl;
+        this.axios = params.axios ?? new Axios({
+            baseURL: this.baseUrl,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+
     }
 }
